c8859a861da4db1552d2f9375c43d723c9f25ac5
Duy Nguyen <dnguyen@stat.wisc.edu>
2014-02-23 17:43:52 -0600

    performed data input, exploratory data, transform log(dspan)

--- Rscript.R
+++ Rscript.R
@@ -12,6 +12,25 @@ source(paste(FilePath, 'fn_Library.R', sep=''))
 
 ################ Input data #################
 setwd(FilePath)
-Filename <- 'hw.dat'
-Data <- read.table(file=Filename, header=FALSE)
+Filename <- 'experiment-table.txt'
+data.sys <- read.table(file=Filename, header=TRUE, quote="'")
+names(data.sys) <- c("size", "dspan", "chunk.order", "fsync", "sync", "chunk.number" )
+# Should I consider size as continuous or catergorical?
+# Consider log-transform dspan -> look much more normal
+data.sys$size <- as.factor(data.sys$size)
+data.sys$chunk.order <- as.factor(data.sys$chunk.order)
+data.sys$fsync <- as.factor(data.sys$fsync)
+data.sys$sync <- as.factor(data.sys$sync)
+str(data.sys)
+############### Exploratory data ###########
+hist(log(data.sys$dspan))
+layout(matrix(c(1:4), nrow = 2, ncol = 2))
+plot(log(dspan) ~ size + chunk.order + fsync + sync,data = data.sys)
+## Do we want factor size in our model? Size seems to have a problem with homogeineity. 
+## Other factors look fine. Based on the boxplot, factors fsync, chunk.order, sync do not 
+## significantly change log(dspan)
+
+############### Initial Analysis:  ###########
+
+
 
