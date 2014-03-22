bdf65e366a3ceace1a5277982d9128ced6ff294e
Duy Nguyen <Duy@duynguyenUW-pc.(none)>
2014-03-22 18:19:41 -0500

    added nested4_4.R

--- nested4_4.R
+++ nested4_4.R
@@ -9,5 +9,5 @@ data.sys <- read.table(file=Filename, header=T, quote="",
                                   "factor","factor","factor","factor","numeric"))
 ## log2 (normalized dspan)
 data.sys$dspan <- data.sys$dspan/data.sys$file.size
-data.sys$file.size <- as.factor(data.sys$size/1024)
+data.sys$file.size <- as.factor(data.sys$file.size/1024)
 data.sys <- as.data.frame(data.sys)
