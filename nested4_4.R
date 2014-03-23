d1d05ef488739e7dc02f4606704616466aac7120
Duy Nguyen <Duy@duynguyenUW-pc.(none)>
2014-03-22 19:39:30 -0500

    updated BLHD4_4.R nested4_4.R

--- nested4_4.R
+++ nested4_4.R
@@ -6,8 +6,16 @@ setwd(FilePath)
 Filename <- 'nested4_4.txt'
 data.sys <- read.table(file=Filename, header=T, quote="",
                        colClasses=c("factor","factor","factor","factor","numeric","factor","factor",
-                                  "factor","factor","factor","factor","numeric"))
-## log2 (normalized dspan)
+                                  "factor","factor","numeric","factor","numeric"))
+## normalized dspan
 data.sys$dspan <- data.sys$dspan/data.sys$file.size
 data.sys$file.size <- as.factor(data.sys$file.size/1024)
+data.sys$disk.size <- as.factor(data.sys$disk.size/2^30)
 data.sys <- as.data.frame(data.sys)
+
+######### Analysis ######################
+## fit model with only main effects
+model <- lm(log2(dspan) ~ (num.chunks + file.size + fullness + dir.id + num.cores + disk.used
+            + disk.size + num.chunks/fsync + num.chunks/sync + num.chunks/chunk.order)^2 ,data = data.sys)
+anova(model)
+
